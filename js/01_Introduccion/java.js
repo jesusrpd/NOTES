0ca079b70447eb5280b20c650cea710966f4faf7
jesusrpd <jesusrodrigo881@gmail.com>
2023-07-14 21:53:29 -0600

    apuntes de css

---
+++ js/01_Introduccion/java.js
@@ -0,0 +1,22 @@
+var resta = 'fsdfsd'
+
+var operación = 10*30
+var operación2 = 10+8
+
+operación = 1
+
+
+//TIPOS DE DATOS SIMPLES
+var string = 'fgsdfsdf'
+var integer = 423
+var float = 2.5
+var blooleans = true // false
+
+
+// TIPOS DE DATOS COMPLEJOS
+console.log('Como estas?')
+
+console.log(operación-operación2);
+
+
+document.getElementById("parrafo").innerHTML = "Hola desde js"
